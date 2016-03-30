fac247207bff6b076a7efb2fa78a9fb7f8b47983
moe-sid <moe.siddique@mail.utoronto.ca>
2016-03-30 15:08:50 -0400

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -1,84 +1,250 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibXVzdGtpbGxtb2UiLCJhIjoiY2lrNzBzYnRwMDEyMHA3a3M1ZGJ1cnI1eSJ9.XlC0AJ2yJYQBRJfgoxXUWA';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2dyNDAwIiwiYSI6ImNpbGhlZTBoeTJkdWt1dG0wdzNlcXpucjUifQ.az2Trz9xKjXx8L5-g1jHWg';
 
-var map	=	new	mapboxgl.Map({		
-	container:'map', //container id	in HTML
-	style:'mapbox://styles/mustkillmoe/cik71shz400e696kp2tvv4slx', //stylesheet	location
-	center:	[-79.34, 43.72],	//starting	point, longitude/latitude
-	zoom: 10.5, //starting	zoom	level
-	maxZoom: 17,
-	minZoom: 10.5
-});
+// var map	=	new	mapboxgl.Map({
+// 	container:'map', //container id	in HTML
+// 	style:'mapbox://styles/ggr400/cilhef5iy007l9pkv7xhk48rc', //stylesheet	location
+// 	center:	[-79.34, 43.72],	//starting	point, longitude/latitude
+// 	zoom: 10.5, //starting	zoom	level
+// 	minZoom: 10.5
+// });
+
+
+if (!mapboxgl.supported()) {
+    alert('Your computer is stupid, because it does not support Mapbox GL');
+} else {
+    var map	=	new	mapboxgl.Map({
+		    container:'map', //container id	in HTML
+		    style:'mapbox://styles/ggr400/cilhef5iy007l9pkv7xhk48rc', //stylesheet	location
+		    center:	[-79.34, 43.72],	//starting	point, longitude/latitude
+		    zoom: 10.7, //starting	zoom	level
+        minZoom: 10.7
+	   });
+};
+
+
 
 map.on ('style.load', function () {
-	map.addSource ('GreenSpace', {
+	map.addSource ('CouncilZones', { // Layer 1
 		'type': 'vector',
-		'url': 'mapbox://mustkillmoe.8if63m8g'
+		'url': 'mapbox://ggr400.8xjq7hc7'
 	});
-	
-	map.addLayer ({
-		'id': 'GreenSpace',
+
+	map.addLayer ({ // L1Z1 (base)
+		'id': 'CZ_base',
 		'type': 'fill',
-		'source': 'GreenSpace',
+		'source': 'CouncilZones',
 		'layout': {},
 		'paint': {
-			'fill-color': 'green',
-			'fill-opacity': 0.4
+			'fill-color': '#fbb4ae',
+			'fill-opacity': 0.2
 		},
-		'source-layer': 'city_green_space_wgs84'
-		
+		'source-layer': 'community_council_wgs84',
+    'filter': ["==", "AREA_NAME", ""]
 	});
-	
-	map.addSource ('Fountains', {
-		'type': 'vector',
-		'url': 'mapbox://mustkillmoe.10jilbpj'
-	});
-	
-	map.addLayer ({
-		'id': 'Fountains',
-		'type': 'circle',
-		'source': 'Fountains',
+
+  map.addLayer({// L1Z1 (hover)
+    'id': 'CZ_hover',
+    'type': 'fill',
+    'source': 'CouncilZones',
+    'interactive': true,
+    'layout': {},
+    'paint': {
+      'fill-color': 'cyan',
+      'fill-opacity': 0.5,
+      'fill-outline-color': 'black'
+    },
+    'source-layer': 'community_council_wgs84',
+    'filter': [ "==", "AREA_NAME", "" ] // start with a filter that doesn't select anything
+  });
+
+	map.addLayer ({ //L1Z1A (Scarborough)
+		'id': 'Scarborough',
+		'type': 'fill',
+		'source': 'CouncilZones',
+		'interactive': true,
 		'layout': {},
 		'paint': {
-			'circle-color': 'cyan',
-			'circle-radius': 2.5,
+			'fill-color': '#fbb4ae',
+			'fill-opacity': 0.2,
+			'fill-outline-color': 'white'
 		},
-		'source-layer': 'pfr_drinking_fountain_wgs84',
+		'source-layer': 'community_council_wgs84',
+		'filter': [ "==", "OBJECTID", 10056625 ]
 	});
-	
-	
+
+  map.addLayer ({// L1Z1B (North York)
+		'id': 'NorthYork',
+		'type': 'fill',
+		'source': 'CouncilZones',
+		'interactive': true,
+		'layout': {},
+		'paint': {
+			'fill-color': '#b3cde3',
+			'fill-opacity': 0.2,
+			'fill-outline-color': 'white'
+		},
+		'source-layer': 'community_council_wgs84',
+		'filter': [ "==", "OBJECTID", 10056641 ]
+	});
+
+  map.addLayer ({// L1Z1C (TorontoEastYork)
+    'id': 'TorontoEastYork',
+    'type': 'fill',
+    'source': 'CouncilZones',
+    'interactive': true,
+    'layout': {},
+    'paint': {
+      'fill-color': '#ccebc5',
+      'fill-opacity': 0.2,
+      'fill-outline-color': 'white'
+    },
+    'source-layer': 'community_council_wgs84',
+    'filter': [ "==", "OBJECTID", 11816481 ]
+  });
+
+  map.addLayer ({// L1Z1D (Etobicoke)
+    'id': 'Etobicoke',
+    'type': 'fill',
+    'source': 'CouncilZones',
+    'interactive': true,
+    'layout': {},
+    'paint': {
+      'fill-color': '#decbe4',
+      'fill-opacity': 0.2,
+      'fill-outline-color': 'white'
+    },
+    'source-layer': 'community_council_wgs84',
+    'filter': [ "==", "OBJECTID", 12995441 ]
+  });
 });
 
-addLayer('Vegetation', 'GreenSpace');
-addLayer('Drinking Stations', 'Fountains');
 
-function addLayer(name, id) {
-    var link = document.createElement('a');
-    link.href = '#';
-    link.className = 'active';
-    link.textContent = name;
 
-    link.onclick = function (e) {
-        e.preventDefault();
-        e.stopPropagation();
+// var popup = new mapboxgl.Popup({
+//     closeButton: false,
+//     closeOnClick: true
+// });
 
-        var visibility = map.getLayoutProperty(id, 'visibility');
+map.on("click", function(e) {
+    var features = map.queryRenderedFeatures(e.point, { layers: ["Scarborough", "NorthYork", "Etobicoke", "TorontoEastYork"] });
+    map.featuresAt(e.point, {
+        radius: 2,
+        includeGeometry: true,
+        layers: ["Scarborough", "NorthYork", "Etobicoke", "TorontoEastYork"]
+    }, function (err, features) {
+          //FIRST: We will change the color of the dissemination area we're hovering over
 
-        if (visibility === 'visible') {
-            map.setLayoutProperty(id, 'visibility', 'none');
-            this.className = '';
-        } else {
-            this.className = 'active';
-            map.setLayoutProperty(id, 'visibility', 'visible');
-        };
-    };
+          if (!err && features.length) { //if no error, and features.length is 'true' (meaning there's stuff in there) then do the following
+              map.flyTo({
+                center: features[0].properties.OBJECTID,
+                zoom: 11.7
+              });
+              map.setFilter("CZ_base", ["==", "AREA_NAME", features[0].properties.AREA_NAME])
+                  .setLngLat(e.lngLat);
+              // popup.setLngLat(e.lngLat)
+              //     .setHTML(features[0].properties.AREA_NAME)
+              //     .addTo(map);
+          }
+          else {
+              map.setFilter("CZ_hover", ["==", "AREA_NAME", ""]);
+              // popup.remove();
+              return;
+          }
+      });
+});
 
-    var layers = document.getElementById('menu');
-    layers.appendChild(link);
-};
+
+
+map.on("mousemove", function(e) {
+      map.featuresAt(e.point, {
+          radius: 2,
+          layers: ["CZ_hover"]
+          }, function (err, features) {
+              if (!err && features.length) { //if no error, and features.length is 'true' (meaning there's stuff in there) then do the following
+
+                  map.getCanvas().style.cursor = (!err && (features[0].properties.AREA_NAME != null)) ? 'pointer' : '';
+                  map.setFilter("CZ_hover", ["==", "AREA_NAME", features[0].properties.AREA_NAME]);
+                  // popup.setLngLat(e.lngLat)
+                  //     .setHTML(features[0].properties.AREA_NAME)
+                  //     .addTo(map);
+                }
+                else {
+                  map.setFilter();
+                  // popup.remove();
+                  return;
+                }
+            });
+});
+
+
+
+	// map.addSource ('business_improvement_areas', { // Layer 2
+	// 	'type': 'vector',
+	// 	'url': 'mapbox://ggr400.b3c9mgpy'
+	// });
+
+	// map.addLayer ({ // Layer 2.1
+	// 	'id': 'business_improvement_areas',
+	// 	'type': 'fill',
+	// 	'source': 'business_improvement_areas',
+	// 	'layout': {},
+	// 	'paint': {
+	// 		'fill-color': 'cyan',
+	// 		'fill-opacity': 0.5,
+	// 		'fill-outline-color': '#ffb366'
+	// 	},
+	// 	'source-layer': 'business_improvement_area_wgs84'
+	// });
+
+	// map.addSource ('Business_development_areas', {
+	// 	'type': 'vector',
+	// 	'url': 'mapbox://ggr400.b3c9mgpy'
+	// });
+
+	// map.addLayer ({
+	// 	'id': 'Development',
+	// 	'type': 'circle',
+	// 	'source': 'Development',
+	// 	'layout': {},
+	// 	'paint': {
+	// 		'circle-color': 'cyan',
+	// 		'circle-radius': 2.5,
+	// 	},
+	// 	'source-layer': 'pfr_drinking_fountain_wgs84',
+	// });
+
+// addLayer('Wards', 'Toronto_Wards');
+// addLayer('Developments', 'business_improvement_areas');
+
+// function addLayer(name, id) {
+//     var link = document.createElement('a');
+//     link.href = '#';
+//     link.className = 'active';
+//     link.textContent = name;
+
+//     link.onclick = function (e) {
+//         e.preventDefault();
+//         e.stopPropagation();
+
+//         var visibility = map.getLayoutProperty(id, 'visibility');
+
+//         if (visibility === 'visible') {
+//             map.setLayoutProperty(id, 'visibility', 'none');
+//             this.className = '';
+//         } else {
+//             this.className = 'active';
+//             map.setLayoutProperty(id, 'visibility', 'visible');
+//         };
+//     };
+
+//     var layers = document.getElementById('menu');
+//     layers.appendChild(link);
+// };
 
 map.addControl(new mapboxgl.Navigation());
 
 
 map.addControl(new mapboxgl.Geocoder());
+
 
 //Lily's test comment
